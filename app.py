bdfe464b2698a4d10fb9fb065bcc861fcb7188a1
Peyton Pridemore <ppridemo@vols.utk.edu>
2025-02-05 11:32:51 -0500

    Added high and low temps

--- app.py
+++ app.py
@@ -12,19 +12,21 @@ def get_weather():
     response = requests.get(URL)
     data = response.json()
     temperature = data["main"]["temp"]
-    #high_temperature = data["main"]["temp"]
+    high_temperature = data["main"]["temp_max"]
+    low_temperature = data["main"]["temp_min"]
+
     rain = data.get("rain", {}).get("1h", 0)
-    return temperature, rain
+    return temperature, high_temperature, low_temperature, rain
 
 @app.route("/")
 def home():
-    temp, rain = get_weather()
+    temp, high, low, rain = get_weather()
     return render_template_string("""
         <h1>Knoxville, TN</h1>
         <h2>Temperature: {{ temp }}°F</h2>
-        <h2>High:{{ }}Low: </h2>
-        <h2>Rain: {{ rain }} mm</h2>
-    """, temp=temp, rain=rain)
+        <h2>High:{{ high }} Low: {{ low }}</h2>
+        <h2>Rain: {{ rain}} mm</h2>
+    """, temp=temp,high = high, low = low, rain=rain)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
