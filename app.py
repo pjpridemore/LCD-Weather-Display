889c76eab36d95f1dd689a7f563dd75d3b348c70
Peyton Pridemore <ppridemo@vols.utk.edu>
2025-02-05 12:08:31 -0500

    Added css and html to better organize and style

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 import requests
 import os
 
@@ -11,22 +11,19 @@ URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}
 def get_weather():
     response = requests.get(URL)
     data = response.json()
-    temperature = data["main"]["temp"]
-    high_temperature = data["main"]["temp_max"]
-    low_temperature = data["main"]["temp_min"]
+    temperature = round(data["main"]["temp"])
+    high_temperature = round(data["main"]["temp_max"])
+    low_temperature = round(data["main"]["temp_min"])
+    description = data["weather"][0]["description"]
+
 
     rain = data.get("rain", {}).get("1h", 0)
-    return temperature, high_temperature, low_temperature, rain
+    return temperature, high_temperature, low_temperature, rain, description
 
 @app.route("/")
 def home():
-    temp, high, low, rain = get_weather()
-    return render_template_string("""
-        <h1>Knoxville, TN</h1>
-        <h2>Temperature: {{ temp }}°F</h2>
-        <h2>High:{{ high }} Low: {{ low }}</h2>
-        <h2>Rain: {{ rain}} mm</h2>
-    """, temp=temp,high = high, low = low, rain=rain)
+    temp, high, low, rain, desc = get_weather()
+    return render_template("index.html", temp=temp, high=high, low=low, rain=rain, desc=desc)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
