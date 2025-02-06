21b6de15a2823b5555daf91c1441f4f228c67355
Peyton Pridemore <ppridemo@vols.utk.edu>
2025-02-06 00:16:46 -0500

    Changed the weather data from current to the 5 day forecast to allow daily highs and lows and precip chance

--- app.py
+++ app.py
@@ -6,27 +6,35 @@ app = Flask(__name__)
 
 API_KEY = os.getenv("WEATHER_API_KEY")
 CITY = "Knoxville"
-URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=imperial"
+lat = 35.962639
+lon = -83.916718
+URL = f"http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API_KEY}&units=imperial"
 
 def get_weather():
+    # Request forecast data
     response = requests.get(URL)
-    data = response.json()
-    temperature = round(data["main"]["temp"])
-    high_temperature = round(data["main"]["temp_max"])
-    low_temperature = round(data["main"]["temp_min"])
-    description = data["weather"][0]["description"]
-    # grab icon code and then generate the image
-    icon_code = data["weather"][0]["icon"]
+    forecast_data = response.json()
+
+    # Checking if API response is valid
+    if "list" not in forecast_data:
+        return None, None, None, None, "Error fetching data", None
+
+    # Get data for the current day
+    current_data = forecast_data["list"][0]
+    current_temp = round(current_data["main"]["temp"])
+    current_high = round(current_data["main"]["temp_max"])
+    current_low = round(current_data["main"]["temp_min"])
+    icon_code = current_data["weather"][0]["icon"]
     icon_url = f"http://openweathermap.org/img/wn/{icon_code}@2x.png"
+    current_precip_chance = round(current_data.get("pop", 0) * 100)  # Convert to percentage
 
-
-    rain = data.get("rain", {}).get("1h", 0)
-    return temperature, high_temperature, low_temperature, rain, description, icon_url
+    return current_temp, current_high, current_low, current_precip_chance, icon_url
 
 @app.route("/")
 def home():
-    temp, high, low, rain, desc, icon= get_weather()
-    return render_template("index.html", temp=temp, high=high, low=low, rain=rain, desc=desc, icon=icon)
+    current_temp, high_temp, low_temp, current_precip_chance,  icon = get_weather()
+    return render_template("index.html", temp=current_temp, high=high_temp, low=low_temp,
+                           precip=current_precip_chance, icon=icon)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
